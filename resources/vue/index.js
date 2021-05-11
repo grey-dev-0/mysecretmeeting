36307550e50aab27d53c99022619b329c30ba378
Mohyaddin Saleh <mo7y.66@gmail.com>
2021-05-11 15:28:28 +0300

    Trying to build components in a bundle.

---
+++ resources/vue/index.js
@@ -0,0 +1,6 @@
+import Vue from 'vue';
+import Peer from './peer.vue';
+import QR from './qr.vue';
+
+Vue.component('qr', QR);
+Vue.component('peer', Peer);
---
+++ resources/vue/qr.vue
@@ -0,0 +1,47 @@
+<template>
+    <div class="col-sm-6 col-md-3 col-lg-2">
+        <div class="card">
+            <div class="card-body">
+                <img class="img-fluid" :src="codeImage || window.basUrl + '/resources/img/no-qr.png'" alt="Qr Code">
+                <div id="copy-url" class="btn btn-outline-primary btn-block" data-clipboard-text="">Copy QR Link</div>
+                <div id="capture-qr" class="btn btn-outline-info btn-block">Capture QR</div>
+                <input type="file" accept="image/*;capture=camera" class="d-none" name="qr_image">
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+var $ = window.$;
+var baseUrl = window.baseUrl
+var ClipboardJS = window.ClipboardJS;
+export default {
+    name: 'QrCode',
+    props: {
+        code: {
+            type: String
+        }
+    },
+    computed: {
+        codeImage: function(){
+            return (this.code == '')? null : (baseUrl + '/qr?c=' + this.code);
+        }
+    },
+    mounted: function(){
+        this.$nextTick(function(){
+            this.$root.initSignalingChannel(this.code);
+        });
+    },
+    methods: {
+        initQrButtons: function(){
+            $('#copy-url').attr('data-clipboard-text', baseUrl + '/?c=' + this.code);
+            new ClipboardJS('#copy-url');
+            $('#capture-qr').on('click', function(){
+                $(this).next().trigger('click');
+            }).next().on('change', function(){
+                // TODO: run ajax to join the given QR room if exists.
+            });
+        }
+    }
+}
+</script>
